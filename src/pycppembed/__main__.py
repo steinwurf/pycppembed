a0e0c55293fad9802388db485b9c7dac6ffe45d4
Jeppe Pihl <jpihl08@gmail.com>
2018-10-05 14:30:53 +0200

    accept globs

--- src/pycppembed/__main__.py
+++ src/pycppembed/__main__.py
@@ -1,21 +1,32 @@
 import argparse
 import os
+import glob
 import pycppembed.generator
 
-def is_valid_file(parser, arg):
+def does_exists(parser, arg):
     if not os.path.exists(arg):
-        parser.error("The file '{}' does not exist!".format(arg))
+        parser.error("'{}' does not exist!".format(arg))
     else:
         return arg
+
+def is_valid_dir(parser, arg):
+    arg = does_exists(parser, arg)
+    if not os.path.isdir(arg):
+        parser.error("'{}' is not a directory!".format(arg))
+    else:
+        return arg
+
+def is_valid_glob(parser, arg):
+    return glob.glob(arg)
 
 def cli():
     parser = argparse.ArgumentParser(
         description='Commandline options',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        'files',
+        'inputs',
         nargs='+',
-        type=lambda arg: is_valid_file(parser, arg),
+        type=lambda arg: is_valid_glob(parser, arg),
         help='file to embed into .cpp file')
     parser.add_argument(
         'class_name',
@@ -23,6 +34,7 @@ def cli():
              'the name of the resulting cpp and hpp file.')
     parser.add_argument(
         'output_path',
+        type=lambda arg: is_valid_dir(parser, arg),
         help='output folder in which the c++ files should be created.')
     parser.add_argument(
         '-ns',
@@ -31,8 +43,12 @@ def cli():
         help='namespace to use')
 
     args = parser.parse_args()
+    files = sum(args.inputs, [])
+    print("Embedding the following files:")
+    for f in sorted(files):
+        print(f)
     pycppembed.generator.generate(
-        files=args.files,
+        files=files,
         class_name=args.class_name,
         output_path=args.output_path,
         namespace=args.namespace)
